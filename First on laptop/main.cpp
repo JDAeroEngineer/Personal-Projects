d610858f12728f221c2eeb4aa2c9bb760091de80
dezhero12 <49578811+dezhero12@users.noreply.github.com>
2020-07-20 12:21:37 -0700

    Signed-off-by: dezhero12 <49578811+dezhero12@users.noreply.github.com>

---
+++ First on laptop/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Justin Diaz
+ *
+ * Created on July 19, 2020, 8:07 PM
+ */
+
+#include <iostream>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<<"hello world";
+    return 0;
+}
+

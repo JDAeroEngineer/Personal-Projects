7f0c82845f91bdab1b36d6f681629fd89143de0c
JDAeroEngineer <justindiazae@gmail.com>
2020-07-20 15:53:59 -0700

    Signed-off-by: JDAeroEngineer <justindiazae@gmail.com>

---
+++ Personal_Project1/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: Justin Diaz
+ * Created on July 00, 2020, 00:00 PM
+ * Purpose:  
+ */
+
+//System Libraries
+
+#include <iostream>   //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants only
+
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+
+int main(int argc, char** argv) {
+    
+    //Set the random number seed here
+    
+    //Declare all variables for this function
+    
+    //Initialize all known Variables
+    
+    //Process inputs to Outputs -> Mapping Process
+    
+    //Display the Inputs/Outputs
+    cout<<"Hello World";
+         
+    //Clean up the code, close files, deallocate memory, etc....
+    
+    //Exist Stage Right
+    
+    return 0;
+}
+                  
